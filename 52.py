c2a04204a42f079013f3dbdad2ab21d847c54b78
suryak24 <36228729+suryak24@users.noreply.github.com>
2018-02-26 15:47:18 +0530

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,25 @@
+a=int(input("Enter the number:"))
+if a<=10:
+  if  a==1:
+     print("one")
+  elif a==2:
+     print("two")
+  elif a==3:
+     print("three")
+  elif a==4:
+     print("four")
+  elif a==5:
+     print("five")
+  elif a==6:
+     print("six")
+  elif a==7:
+     print("sevan")
+  elif a==8:
+     print("eight")
+  elif a==9:
+     print("nine")
+  elif a==10:
+     print("ten")
+
+
+    
