a26caf655b9bfd9fc893f4a4d6d362cc8d982929
suryak24 <36228729+suryak24@users.noreply.github.com>
2018-03-09 16:20:54 +0530

    Create 81.py

---
+++ 81.py
@@ -0,0 +1,12 @@
+a1=int(input("Enter the a1 value:"))
+b1=int(input("Enter the b1 value:"))
+a2=int(input("Enter the a2 value:"))
+b2=int(input("Enter the b2 value:"))
+a3=int(input("Enter the a3 value:"))
+b3=int(input("Enter the b3 value:"))
+n1=a1-b1
+n2=a2-b2
+n3=a3-b3
+print(n1)
+print(n2)
+print(n3)
