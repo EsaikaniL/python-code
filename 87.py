d94eb29c5a0a594e1302dc90865036200bd84931
suryak24 <36228729+suryak24@users.noreply.github.com>
2018-03-12 16:17:14 +0530

    Create 87.py

---
+++ 87.py
@@ -0,0 +1,15 @@
+n=int(input("Enter the integer1:"))
+m=int(input("Entre the integer2:"))
+a=[]
+b=[]
+c=[]
+for i in range(1,n+1):
+    if n%1==0:
+        a.append(i)
+for i in range(1,m+1):
+    if m%i==0:
+        b.append(i)
+for i in a:
+    if i  in b:
+        c.append(i)
+print("GCD:",max(c))
