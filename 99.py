abe6c7c8dae9110940fd4e56b5c91cab1f4898a6
suryak24 <36228729+suryak24@users.noreply.github.com>
2018-03-14 16:51:12 +0530

    Update 99.py

--- 99.py
+++ 99.py
@@ -1,5 +1,5 @@
-a=int(input("Enter the n value:"))
-b=int(input("Enter the m value:"))
-c=int(input("Enter the l value:"))
+a=int(input("Enter the a value:"))
+b=int(input("Enter the b value:"))
+c=int(input("Enter the c value:"))
 ans=a*b%c
 print(ans)
