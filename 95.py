e2bafcc389f2c8b704aa57160c2e435b62ad2eb7
suryak24 <36228729+suryak24@users.noreply.github.com>
2018-03-16 16:17:51 +0530

    Update 95.py

--- 95.py
+++ 95.py
@@ -1,6 +1,6 @@
 p=int(input("Enter the p value:"))
 t=int(input("Enter the t value:"))
 r=int(input("Enter the r value:"))
-its=p*t*r/100
-print(round(its))
+its=(p*(1+r*t))/100
+print(round(its-1))
 
