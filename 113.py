293e5ca64b82186a0e7232ff1e0d26e950b39e15
suryak24 <36228729+suryak24@users.noreply.github.com>
2018-03-28 16:10:10 +0530

    Create 113.py

---
+++ 113.py
@@ -0,0 +1,13 @@
+n=int(input("Enter N value:"))
+k=int(input("Enter K value:"))
+a=[]
+count=0
+for i in range(n):
+	m=int(input())
+	a.append(m)
+
+for i in a:
+    if(i==m):
+	    count=count+1
+print(count)
+
