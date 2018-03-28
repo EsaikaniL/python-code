0ce9d08ff71976d50dfb3e0628b83b7cba011004
suryak24 <36228729+suryak24@users.noreply.github.com>
2018-03-28 16:01:10 +0530

    Create 112.py

---
+++ 112.py
@@ -0,0 +1,12 @@
+n=int(input("Enter N value:"))
+k=int(input("Enter K value:"))
+print("Enter the Array:")
+a=[]
+for i in range(n):
+	m=int(input())
+	a.append(m)
+
+if(k==m):
+	print("Yes")
+else:
+	print("No")
